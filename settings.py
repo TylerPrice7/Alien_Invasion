96ebed801a560207c8cd0fca8611541f7868fe9a
TpFire <tyler101price@gmail.com>
2023-02-07 23:34:56 -0500

    First Settings class

---
+++ settings.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Settings():
+    def _init__(self):
+        screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        bg_color = (230, 230, 230)
+        ship_speed = 1
+
+
