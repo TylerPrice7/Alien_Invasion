463782b2035f001ea63e368bf409f4b335266170
TpFire <tyler101price@gmail.com>
2023-02-08 12:35:30 -0500

    Added more attributes and made accessible

--- settings.py
+++ settings.py
@@ -1,9 +1,11 @@
 import pygame
 
 class Settings():
-    def _init__(self):
-        screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-        bg_color = (230, 230, 230)
-        ship_speed = 1
+    def __init__(self):
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.screen_width = 1200
+        self.screen_height = 700 
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 1
 
 
