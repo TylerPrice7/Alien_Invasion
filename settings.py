5c1724d1f863b08683605028dde71bee75ac95c4
TpFire <tyler101price@gmail.com>
2023-02-11 00:21:06 -0500

    Allowed custom speeds

--- settings.py
+++ settings.py
@@ -6,6 +6,6 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 700 
         self.bg_color = (230, 230, 230)
-        self.ship_speed = 1
+        self.ship_speed = 1.2
 
 
