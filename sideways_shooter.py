ad5a9ffe0655e6c6a232a2e34853be37b87db33a
TpFire <tyler101price@gmail.com>
2023-02-12 17:49:48 -0500

    Made sideways, left to right

--- sideways_shooter.py
+++ sideways_shooter.py
@@ -1,7 +1,8 @@
 import pygame
 import sys
-from settings import Settings
-from ship import Ship
+from sideways_settings import Settings
+from sideways_ship import Ship
+from sideways_bullet import Bullet
 
 class AlienInvasion():
     def __init__(self):
@@ -9,8 +10,9 @@ class AlienInvasion():
         self.settings = Settings()
         self.screen = self.settings.screen
         #self.screen = pygame.display.set_mode((self.settings.screen_width,
-#self.settings.screen_height))
+            #self.settings.screen_height))
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         pygame.display.set_caption("Alien Rocket")
 
     def _check_events(self):
@@ -27,6 +29,8 @@ class AlienInvasion():
         """Sets movement boolean if key is being pressed"""
         if event.key == pygame.K_q:
             sys.exit()
+        if event.key == pygame.K_SPACE:
+            self._fire_bullet()
         if event.key == pygame.K_LEFT:
             self.ship.move_left = True
         if event.key == pygame.K_RIGHT:
@@ -47,16 +51,33 @@ class AlienInvasion():
         if event.key == pygame.K_DOWN:
             self.ship.move_down = False
     
+    def _fire_bullet(self):
+        """Create a new bullet and adds to bullets group"""
+        if len(self.bullets) < self.settings.bullet_max:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Removes bullets that have exited the screen perimeter"""
+        for bullet in self.bullets.copy():
+            if bullet.rect.left >= self.ship.screen_rect.right:
+                self.bullets.remove(bullet)
+
     def _update_screen(self):
-        self.screen.fill(self.settings.bg_color) # Fill screen with bg color
-        self.ship.blitme() # Load ship image
-        pygame.display.flip() # Show the screen
+        """Updates screen with background, ship, and bullets"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        for bullet in self.bullets:
+            bullet.draw_bullet()
+        pygame.display.flip()
 
     def run_game(self):
         """Runs main loop of the game"""
         while True:
             self._check_events()
             self.ship.update_movement()
+            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
 
 if __name__ == '__main__':
