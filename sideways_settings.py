497964a144132839e7c582288872a68a52def25c
TpFire <tyler101price@gmail.com>
2023-02-12 17:51:26 -0500

    Settings class for sideways_shooter

---
+++ sideways_settings.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Settings():
+    def __init__(self):
+        """Initializes settings for the screen, background color, ship,"""
+        """ and bullets"""
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.screen_width = 1200
+        self.screen_height = 700 
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 1.2
+        self.bullet_color = (60, 60, 60)
+        self.bullet_height = 10
+        self.bullet_width = 30
+        self.bullet_speed = 1.0
+        self.bullet_max = 5
+
+
