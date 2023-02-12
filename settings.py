fde1140d688c19e0b9c094f43d46c665c3b96053
TpFire <tyler101price@gmail.com>
2023-02-12 17:48:16 -0500

    Added bullet settings

--- settings.py
+++ settings.py
@@ -7,5 +7,10 @@ class Settings():
         self.screen_height = 700 
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.2
+        self.bullet_color = (60, 60, 60)
+        self.bullet_height = 30
+        self.bullet_width = 10
+        self.bullet_speed = 1.0
+        self.bullet_max = 4
 
 
