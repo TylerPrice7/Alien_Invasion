7b3712a995372a33af5c99be91472037986ee05e
TpFire <tyler101price@gmail.com>
2023-02-25 13:29:14 -0500

    Updates y value of aliens and checks edges

--- sideways_alien.py
+++ sideways_alien.py
@@ -7,10 +7,10 @@ class Alien(Sprite):
         """Initializes alien sprite"""
         super().__init__()
         self.screen = ai_game.screen
-        self.alien_speed = 4
+        self.settings = ai_game.settings
 
         # Load in Alien image and image size
-        self.image = pygame.image.load('Alien_Invasion/images/sideways_alien.bmp')
+        self.image = pygame.image.load('images/sideways_alien.bmp')
         self.rect = self.image.get_rect()
 
         # Set Alien to load near top left of screen
@@ -19,5 +19,14 @@ class Alien(Sprite):
 
         # Finer control of vertical movement
         self.y = float(self.rect.y)
-        
-        
+
+    def check_edges(self):
+        """Checks if alien is at an edge of the screen and return True if so"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.bottom >= screen_rect.bottom or self.rect.top <= 0:
+            return True
+
+    def update(self):
+        """Moves alien up or down the screen, depending on change_direction"""
+        self.y += self.settings.alien_speed * self.settings.change_direction
+        self.rect.y = self.y
