7833f62c4900b56a271bdc9d591e9182b8a7c17f
TpFire <tyler101price@gmail.com>
2023-03-18 15:32:52 -0400

    Uses path module for getting image

--- ship.py
+++ ship.py
@@ -1,15 +1,18 @@
 import pygame
+from pygame.sprite import Sprite
+from pathlib import Path
 
-class Ship():
+class Ship(Sprite):
     """Controls the ship in the sideways_shooter"""
     def __init__(self, ai_game):
         """Initialize image size and location"""
+        super().__init__()
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
 
         # Load image file into game
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = Path(__file__).parent / "images/ship.bmp"
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
 
